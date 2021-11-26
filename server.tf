972aa99ddeaa6f935d3ba558e9aac0c94d3a2a54
vishalshetty07 <95043633+vishalshetty07@users.noreply.github.com>
2021-11-26 19:11:18 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,7 +4,7 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.small"
-  key_name = "gds-import"
+  key_name = "vishalKey"
 
   tags = {
     Name = "Gagan-devops-server"
